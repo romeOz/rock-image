3e30c89cf7910d4f7c3a40d90c65e03c3424e916
romeOz <serggalka@gmail.com>
2015-11-09 07:52:53 +0300

    Testing Gmagick.

--- tests/ImageGmagickTest.php
+++ tests/ImageGmagickTest.php
@@ -11,13 +11,24 @@ class ImageGmagickTest extends AbstractImageTest
 {
     protected function setUp()
     {
-        if (!class_exists('Gmagick')) {
+        if (!class_exists('\Gmagick')) {
             $this->markTestSkipped('Skipping ImageGmagickTest, Gmagick is not installed');
         } else {
             Image::setImagine(null);
             Image::$driver = Image::DRIVER_GMAGICK;
             parent::setUp();
         }
+    }
+
+    public function testFrame()
+    {
+        $frameSize = 5;
+        $original = Image::getImagine()->open($this->imageFile);
+        $originalSize = $original->getSize();
+        $img = Image::frame($this->imageFile, $frameSize, '666', null);
+        $size = $img->getSize();
+
+        $this->assertEquals($size->getWidth(), $originalSize->getWidth() + ($frameSize * 2));
     }
 
     protected function isFontTestSupported()
